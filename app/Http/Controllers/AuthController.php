fa88099c3878189d0e25170fb4547437a8a9a69e
horvathg13 <horvathgabor338@gmail.com>
2023-12-10 23:48:18 +0100

    new Auth funcks dev

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PasswordResets;
 use App\Models\Statuses;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
-
+use Illuminate\Support\Str;
 class AuthController extends Controller
 {
     public function register(Request $request){
@@ -119,5 +121,131 @@ class AuthController extends Controller
             
         }
         
+    }
+
+    public function createUser(Request $request){
+        $validator = Validator::make($request->all(), [
+            "fname"=>"required",
+            "lname"=>"required",
+            "email"=>"required|email|unique:users,email",
+            "psw"=>"required"
+        ],[
+            "fname.required"=>__('validation.custom.fname.required'),
+            'lname.required' => __('validation.custom.lname.required'),
+            'email.required' => __('validation.custom.email.required'),
+            'email.unique' =>  __('validation.custom.email.unique'),
+            'psw.required' => __('validation.custom.psw.required'),
+
+        ]);
+        if($validator->fails()){
+            $validatorResponse=[
+                "validatorResponse"=>$validator->errors()->all()
+            ];
+            return response()->json($validatorResponse,422);
+        }
+
+        $token= Str::random(60);
+        DB::transaction(function () use ($request, $token){
+            
+            
+            $passwordReset = PasswordResets::create([
+                "email"=>$request->email,
+                "token"=>$token
+            ]);
+
+            if($passwordReset){
+                $findSuspendedStatus=Statuses::where("status","Suspended")->pluck('id')->first();
+                if(!$findSuspendedStatus){
+                    throw new Exception('Database error');
+                }
+    
+                $user = User::create([
+                    "first_name"=> $request->fname,
+                    "last_name"=>$request->lname,
+                    "email"=>$request->email,
+                    "password"=> bcrypt($request->psw),
+                    "status"=>$findSuspendedStatus
+                ]);
+
+               
+            }else{
+                throw new Exception('Database error occured during password reset');
+            }
+
+            $success=[
+                "message"=>"User Create successful",
+                "link"=>"localhost:3000/generated-user/$token"
+            ];
+            
+            return response()->json($success);
+            
+            
+        });
+
+        $success=[
+            "message"=>"User Create successful",
+            "link"=>"localhost:3000/generated-user/$token"
+        ];
+        
+        return response()->json($success);
+    }
+
+    public function passwordReset($token){
+        if($token){
+            $findToken= PasswordResets::where("token",$token)->first();
+
+            if($findToken){
+                $findUser=User::where("email", $findToken['email'])->first();
+                
+                if($findUser){
+                    $success=[
+                        "id"=>$findUser["id"],
+                        "firstname"=>$findUser["first_name"],
+                        "lastname"=>$findUser["last_name"],
+                        "email"=>$findUser["email"]
+                    ];
+
+                    return response()->json($success);
+                }else{
+                    throw new Exception("User is not found");
+                }
+            }
+        }else{
+            throw new Exception("Token is missing");
+        }
+        
+    }
+
+    public function resetPassword(Request $request){
+        $validator = Validator::make($request->all(), [
+            "userId"=>"required|exists:users,id",
+            "psw"=>"required",
+        ]);
+        if($validator->fails()){
+            $validatorResponse=[
+                "validatorResponse"=>$validator->errors()->all()
+            ];
+            return response()->json($validatorResponse,422);
+        }
+        DB::transaction(function () use ($request){
+
+            $findActiveStatus=Statuses::where("status","Active")->pluck('id')->first();
+            if(!$findActiveStatus){
+                throw new Exception('Database error');
+            }
+            $findUser=User::where("id", $request->userId)->first();
+            $findUser->update([
+                "password"=> bcrypt($request->psw),
+                "status"=>$findActiveStatus
+            ]);
+            $findinResetPassword=PasswordResets::where("email", $findUser['email'])->exists();
+            if($findinResetPassword){
+                PasswordResets::where("email", $findUser['email'])->first()->delete();
+            }
+            if($findUser){
+                $success="Pasword Reset was successful";
+                return response()->json([$success,200]);
+            }
+        });
     }
 }
