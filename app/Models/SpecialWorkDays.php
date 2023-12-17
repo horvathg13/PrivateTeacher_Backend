84ab7fac1bfe70ac7322b2c2fe2759162cd04f6f
horvathg13 <horvathgabor338@gmail.com>
2023-12-17 22:06:17 +0100

    SpecialWorkdDays Model modif (spelling mistake)

---
+++ app/Models/SpecialWorkDays.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SpecialWorkDays extends Model
+{
+    use HasFactory;
+    protected $table='special_work_days';
+    protected $fillable=[
+        'name',
+        'start',
+        'end',
+        'school_id',
+        'school_year_id'
+    ];
+}
