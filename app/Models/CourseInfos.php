e562271dac8d1b9a5da5683eb00729282707df46
horvathg13 <horvathgabor338@gmail.com>
2024-02-28 22:01:13 +0100

    courseInfos model modified

--- app/Models/CourseInfos.php
+++ app/Models/CourseInfos.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class CourseInfos extends Model
 {
@@ -13,12 +14,18 @@ class CourseInfos extends Model
         'name',
         'subject',
         'student_limit',
-        'minutes/lesson',
+        'minutes_lesson',
         'min_teaching_day',
         'double_time',
         'course_price_per_lesson',
-        'status', 
+        'status_id', 
         'school_id',
         'school_year_id'
     ];
+    public $timestamps = false;
+
+    public function status():HasOne 
+    {
+        return $this->hasOne(Statuses::class, "id", "status_id");
+    }
 }
