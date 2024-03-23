f254f4d7ec00247b8ea27cdbcbc5fd10cad153ee
horvathg13 <horvathgabor338@gmail.com>
2024-03-23 21:54:25 +0100

    CourseInfos.php modified

--- app/Models/CourseInfos.php
+++ app/Models/CourseInfos.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class CourseInfos extends Model
@@ -18,14 +20,23 @@ class CourseInfos extends Model
         'min_teaching_day',
         'double_time',
         'course_price_per_lesson',
-        'status_id', 
+        'status_id',
         'school_id',
         'school_year_id'
     ];
     public $timestamps = false;
 
-    public function status():HasOne 
+    public function status():BelongsTo
     {
-        return $this->hasOne(Statuses::class, "id", "status_id");
+        return $this->BelongsTo(Statuses::class);
+    }
+
+    public function school():BelongsTo
+    {
+        return $this->BelongsTo(Schools::class);
+    }
+    public function label():BelongsToMany
+    {
+        return $this->BelongsToMany(Labels::class, 'course_labels', 'course_id','label_id');
     }
 }
