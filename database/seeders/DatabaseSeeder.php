c07dc81942cca43d3f8fe66d9db9fb2192176184
hgabor13 <horvathgabor338@gmail.com>
2024-06-07 10:31:24 +0200

    sedders modified

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            StatusSeeder::class,
-            PaymentPeriodSeeder::class,
             RoleSeeder::class
         ]);
     }
