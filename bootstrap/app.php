9f905219fcc0bf43f9753ea9ca7ae9ac8627ed55
hgabor13 <horvathgabor338@gmail.com>
2024-06-11 17:17:32 +0200

    Middleware registered in bootstrap.php/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->appendToGroup('api', \App\Http\Middleware\LocaleMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
