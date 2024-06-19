7a09deaef04179684c22908bac7ee58e4cb55a71
hgabor13 <horvathgabor338@gmail.com>
2024-06-19 23:18:54 +0200

    new enum translation files added

---
+++ lang/en-GB/enums.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "ACTIVE"=>"Active",
+    "SUSPENDED"=>"Suspended",
+    "DELETED"=>"Deleted",
+    "BANNED"=>"Banned",
+    "PER_LESSON"=>"Per lesson",
+    "MONTHLY"=>"Monthly",
+    "YEARLY"=>"Annual",
+    "HALF_YEAR"=>"Half year",
+];
---
+++ lang/hu-HU/enums.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "ACTIVE"=>"Aktív",
+    "SUSPENDED"=>"Felfüggesztve",
+    "DELETED"=>"Törölve",
+    "BANNED"=>"Tiltva",
+    "PER_LESSON"=>"Alkalmanként",
+    "MONTHLY"=>"Havonta",
+    "YEARLY"=>"Évente",
+    "HALF_YEAR"=>"Félévente",
+];
