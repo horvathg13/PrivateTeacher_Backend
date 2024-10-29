5743c503c4a2a4faadb5f084034289c88f32833c
hgabor13 <horvathgabor338@gmail.com>
2024-10-29 18:28:44 +0100

    new functions in the model

--- app/Models/TeacherCourseRequests.php
+++ app/Models/TeacherCourseRequests.php
@@ -4,6 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class TeacherCourseRequests extends Model
 {
@@ -17,4 +21,23 @@ class TeacherCourseRequests extends Model
         'notice'
     ];
     public $timestamps=true;
+
+    public function childInfo(): HasOne{
+
+        return $this->hasOne(Children::class, 'id', 'child_id');
+    }
+    public function parentInfo():hasOneThrough{
+        return $this->hasOneThrough(User::class, ChildrenConnections::class,'child_id', 'id', 'child_id','parent_id');
+    }
+
+    public function courseInfo(): hasOne{
+        return $this->hasOne(CourseInfos::class,'id', 'teacher_course_id');
+    }
+
+    public function courseNamesAndLangs(): HasMany
+    {
+        return $this->hasMany(CourseLangsNames::class, 'course_id', 'teacher_course_id');
+    }
+
+
 }
