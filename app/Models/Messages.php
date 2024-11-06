f2a1d43814f07b4a77c9d3b1812cf5de9a63a224
hgabor13 <horvathgabor338@gmail.com>
2024-11-06 10:28:34 +0100

    model modified

--- app/Models/Messages.php
+++ app/Models/Messages.php
@@ -4,15 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class Messages extends Model
 {
     use HasFactory;
     protected $table='messages';
-    protected $fillabel=[
+    protected $fillable=[
         'teacher_course_request_id',
         'sender_id',
         'receiver_id',
         'message'
     ];
+
+    public function courseInfo(): HasOne{
+        return $this->hasOne(CourseInfos::class,'id','teacher_course_request_id');
+    }
+
+    public function senderInfo(): HasOne{
+        return $this->hasOne(User::class,'id','sender_id');
+    }
+
+    public function childInfo(): HasOneThrough{
+        return $this->hasOneThrough(Children::class, TeacherCourseRequests::class, 'teacher_course_request_id', 'id', 'child_id', 'child_id');
+    }
 }
