79af3204d28b6e2ba1d9adb2622a5c9d2efa63fd
hgabor13 <horvathgabor338@gmail.com>
2024-12-28 09:14:12 +0100

    getnotifications modified

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Notifications;
 use Illuminate\Support\Facades\DB;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use function PHPUnit\Framework\isEmpty;
 
 class NotificationController extends Controller
 {
@@ -12,7 +13,14 @@ class NotificationController extends Controller
     {
         $user=JWTAuth::parseToken()->authenticate();
         $getNotifications=Notifications::where(['receiver_id'=>$user->id])->orderBy('created_at', 'desc')->get();
-
+        if($getNotifications->isEmpty()){
+            $empty=[
+                "empty"=>true,
+                "message"=>"empty-notification",
+                "created_at"=>now()
+            ];
+            return response()->json([$empty]);
+        }
         return response()->json($getNotifications);
     }
 
