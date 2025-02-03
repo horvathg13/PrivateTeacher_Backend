af974e128b4e0068c367a5b4943968a66dae5cca
hgabor13 <horvathgabor338@gmail.com>
2025-02-03 11:58:50 +0100

    new migration file added

---
+++ database/migrations/2025_01_29_081253_teacher_course_requests_modify.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::dropColumns('teacher_course_requests', ['notice', 'status']);
+
+    }
+
+    public function down(): void
+    {
+        Schema::table('teacher_course_requests', function (Blueprint $table) {
+            //
+        });
+    }
+};
