03e6212e9a47533d199da11a1e7294b79865975a
hgabor13 <horvathgabor338@gmail.com>
2025-02-08 15:56:37 +0100

    language seeder file modified

--- database/seeders/LanguageSelectSeeder.php
+++ database/seeders/LanguageSelectSeeder.php
@@ -10,13 +10,13 @@ class LanguageSelectSeeder extends Seeder
 {
     public function run(): void
     {
-        $json=file_get_contents("database/seeders/languages.json");
+        $json=file_get_contents("database/seeders/new_languages.json");
         $languages=json_decode($json,true);
 
         foreach ($languages as $language){
             DB::transaction(function () use($language){
                 Languages::create([
-                    "value"=>strtoupper($language["value"]),
+                    "value"=>$language["value"],
                     "label"=>$language["label"]
                 ]);
             });
