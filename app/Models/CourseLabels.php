6d5a4a41b70586f8619912a7f8d064dc4d9cc6b2
hgabor13 <horvathgabor338@gmail.com>
2025-02-24 08:41:40 +0100

    new fn added in the model

--- app/Models/CourseLabels.php
+++ app/Models/CourseLabels.php
@@ -4,13 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class CourseLabels extends Model
 {
     use HasFactory;
     public $timestamps = false;
     protected $table='course_labels';
-    protected $fillable=['course_id', 'label_id'];
+    protected $fillable=['course_id', 'label_id', 'language_id'];
 
-    
+
+    public function getCourseLabels():HasOne
+    {
+        return $this->hasOne(Labels::class, "id", "label_id");
+    }
+
 }
